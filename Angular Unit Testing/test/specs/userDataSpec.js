e5c5aaf465f55bf6b74e93578e373287547f25f0
bfout <brian.fout@wowway.com>
2015-01-14 10:39:53 -0500

    Added two new unit tests.

--- Angular Unit Testing/test/specs/userDataSpec.js
+++ Angular Unit Testing/test/specs/userDataSpec.js
@@ -17,7 +17,7 @@ describe('userData', function () {
     beforeEach(module("eventsApp"));
 
     beforeEach(function () {
-        mockUserResource = sinon.stub({ get: function () { } });
+        mockUserResource = sinon.stub({ get: function () { },  save: function () { } });
 
         module(function ($provide) {
             $provide.value('userResource', mockUserResource);
@@ -25,10 +25,25 @@ describe('userData', function () {
     });
 
     describe('getUser', function() {
-        it('should call getresource.get with the username', inject(function (userData) {
+        it('should call userResource.get with the username', inject(function (userData) {
             userData.getUser('bob');
 
             expect(mockUserResource.get.args[0][0]).toEqual({ userName: 'bob' });
+        }));
+
+        it('should return whatever userResource.get returns', inject(function (userData) {
+            mockUserResource.get.returns('angular rocks');
+            var result = userData.getUser('bob');
+
+            expect(result).toBe('angular rocks');
+        }));
+    });
+
+    describe('save', function () {
+        it('should call userResource.save with the same parameter', inject(function (userData) {
+            userData.save('some value');
+
+            expect(mockUserResource.save.calledWith('some value')).toBe(true);
         }));
     });
 });
